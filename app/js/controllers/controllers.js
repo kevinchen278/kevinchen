63ae499ae2d43272b479eb1d95cb18ef3d9d2346
kevinchen278 <kevin.chen278@gmail.com>
2015-05-22 23:41:30 -0400

    update controllers.js and services.js files

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -1,18 +1,13 @@
 'use strict';
 
 kevinApp.controller('InfoCtrl', ['ResumeService','$scope', function(ResumeService,$scope){
-	var q = ResumeService.getInfo()
-			.success(function(data,status){
-				$scope.data = data;
-				console.log('success');
-				console.log(data);
-			})
-			.error(function(data,status){
-				console.log('error req.');
+	      ResumeService.getInfo()
+			.then(function(response){
+				$scope.data = response.data;
 			})
 }]);
 
 kevinApp.controller('resumeCtrl', function(){
   		return {};
   	
-  })
+})
--- app/js/services/Services.js
+++ app/js/services/Services.js
@@ -2,15 +2,22 @@
 var kevinApp = angular.module('kevinApp');
 
 kevinApp.service('ResumeService', ['$http', function($http){
-	var ResumeService = {};
-	ResumeService.getInfo = function(){
+	 var ResumeService = {};
+	 ResumeService.getInfo = function(){
 		return $http({
 				method: 'get',
 				url:'./json/kevin.json'
-			});
-	}
+			})
+		    .success(function(data,status,headers,config){
+		    	return data;
+		      })
+		    .error(function(data, status, headers, config){
+		    	console.log('error on req.');
+		    })
 
-	return ResumeService;
+	 };
+
+	 return ResumeService;
 
 }]);
 
